1b89e0fac47edd76cdae7a213b67d77abc7ebc50
SpidaAfrica <spidaafrica@gmail.com>
2025-05-02 12:12:49 +0100

    Update page.tsx

--- app/passenger/page.tsx
+++ app/passenger/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { AuthGuard } from "@/components/AuthGuard";
 
 export default function BasePage() {
-  return <div></div>;
+  return(
+    <AuthGuard>
+      <div></div>
+    </AuthGuard>
+  );
 }
