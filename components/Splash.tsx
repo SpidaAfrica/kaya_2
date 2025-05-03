bfae5a8efece5dea2dfc5753fc25b024ec606e0a
SpidaAfrica <spidaafrica@gmail.com>
2025-05-03 21:34:09 +0100

    Update Splash.tsx

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -5,7 +5,14 @@ import React from "react";
 export default function Splash() {
   return (
     <div className="w-full h-svh flex items-center justify-center bg-primary">
-      <Image src={IconLogo} alt="icon-logo" />
+      <Image
+        src={IconLogo}
+        alt="icon-logo"
+        width={150} // Adjust width as needed
+        height={150}
+        className="w-24 sm:w-36 md:w-40 lg:w-44 object-contain"
+        priority
+      />
     </div>
   );
 }
