7ea0972cfc096573f104467dc08e0bb462dd4536
SpidaAfrica <spidaafrica@gmail.com>
2025-05-04 12:23:56 +0100

    Update layout.tsx

--- app/passenger/home/(layout-1)/layout.tsx
+++ app/passenger/home/(layout-1)/layout.tsx
@@ -15,6 +15,7 @@ import { useMemo } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { ArrowRight } from "lucide-react";
 import { Hero } from "@/components/Hero";
+import useAuth from "@/components/useAuth";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   const isAuthenticated = useAuth(); // This will handle the redirect if not authenticated
