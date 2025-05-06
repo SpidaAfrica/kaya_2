c786c1a11040affbafd69034fedf0041675efa67
SpidaAfrica <spidaafrica@gmail.com>
2025-05-06 14:08:37 +0100

    Update DeliveryDetails.tsx

--- components/Overlays/DeliveryDetails.tsx
+++ components/Overlays/DeliveryDetails.tsx
@@ -174,7 +174,7 @@ export function DeliveryDetails({
     }
 
     try {
-      const response = await fetch("https://jbuit.org/api/create-package.php", {
+      const response = await fetch("https://spida.africa/kaya-api/create-package.php", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({
