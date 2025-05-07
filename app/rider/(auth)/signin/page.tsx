d79fa4920ccd9c2254118aa4f9e69511cfae890a
SpidaAfrica <spidaafrica@gmail.com>
2025-05-07 16:28:09 +0100

    Update page.tsx

--- app/rider/(auth)/signin/page.tsx
+++ app/rider/(auth)/signin/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
 import React, { useEffect, useState } from "react";
-import React from "react";
 import Link from "next/link";
 import AuthForm from "../riderAuth";
 import { Nigeria, Lock, Eye } from "@/components/svgs";
