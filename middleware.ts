430fc82aa0018be6b71d9758a16715e741e88bcf
SpidaAfrica <spidaafrica@gmail.com>
2025-05-08 16:13:09 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("kaya_token")?.value;
 
   if (!token) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
   }
 
   return NextResponse.next();
