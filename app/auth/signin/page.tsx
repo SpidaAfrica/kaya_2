a27c0f7a57cbb2aaff7cac98d148ebdb0a3470ec
SpidaAfrica <spidaafrica@gmail.com>
2025-05-10 12:01:24 +0100

    Update page.tsx

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -64,6 +64,7 @@ export default function SignInPage() {
         sessionStorage.setItem("imageUrl", user.image_url || "");
         sessionStorage.setItem("fullName", user.fullName || "");
         sessionStorage.setItem("phoneNumber", user.phone || "");
+        sessionStorage.getItem("role","sender");
       }
 
       // ✅ Redirect to home
