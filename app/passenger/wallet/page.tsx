b301bd365aa83290afa60e42b7811f3d468ddf0b
SpidaAfrica <spidaafrica@gmail.com>
2025-05-28 10:50:15 +0100

    Update page.tsx

--- app/passenger/wallet/page.tsx
+++ app/passenger/wallet/page.tsx
@@ -344,7 +344,6 @@ export default function WalletPage() {
                       </svg>
                       <span className="ml-1">Deposit</span>
                     </Button>
-                  </div>
                 </div>
               </div>
             </div>
