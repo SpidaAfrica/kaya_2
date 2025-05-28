5f9954426f041359151569dfa919ecdb357faa2c
SpidaAfrica <spidaafrica@gmail.com>
2025-05-28 18:14:43 +0100

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="w-full h-svh flex items-center justify-center bg-primary">
+    <div className="w-1/2 h-[50svh] flex items-center justify-center bg-primary">
       <Image src={IconLogo} alt="icon-logo" />
     </div>
   );
