49e57d111394a48781a144593e704a0f320a28c2
SpidaAfrica <spidaafrica@gmail.com>
2025-05-28 18:30:34 +0100

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,11 @@
-/*import { IconLogo } from "@/assets";
+import { IconLogo } from "@/assets";
 import Image from "next/image";
 import React from "react";
 
 export default function Loading() {
   return (
-    <div className="w-1/2 h-[50svh] flex items-center justify-center bg-primary">
+    <div className="w-full h-svh flex items-center justify-center bg-primary">
       <Image src={IconLogo} alt="icon-logo" />
     </div>
   );
-}*/
+}
